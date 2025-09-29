a0b4cf104fcd9bc442416b632329d852bb2261c4
ErmolaevDmitrii <ermolaev.dn@phystech.edu>
2025-09-29 23:11:22 +0300

    [master] initial commit

---
+++ SDL/sim.h
@@ -0,0 +1,16 @@
+#ifndef SIM_H
+#define SIM_H
+
+#define SIM_X_SIZE 512
+#define SIM_Y_SIZE 256
+
+#ifndef __sim__
+void simInit();
+void app();
+void simExit();
+void simFlush();
+void simPutPixel(int x, int y, int argb);
+int simRand(void);
+#endif // __sim__
+
+#endif // SIM_H
