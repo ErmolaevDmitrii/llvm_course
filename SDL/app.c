42fa5971b235e0e4f8770a8e21904b957d1889af
ErmolaevDmitrii <ermolaev.dn@phystech.edu>
2025-09-30 12:29:37 +0300

    [master] Live game implementation

--- SDL/app.c
+++ SDL/app.c
@@ -1,10 +1,95 @@
 #include "sim.h"
 
+int game_field_1[FIELD_X_SIZE][FIELD_Y_SIZE];
+int game_field_2[FIELD_X_SIZE][FIELD_Y_SIZE];
+
+int (*game_field     )[FIELD_X_SIZE][FIELD_Y_SIZE];
+int (*prev_game_field)[FIELD_X_SIZE][FIELD_Y_SIZE];
+
+void draw_rectangle(int x, int y) {
+  if((*game_field)[x][y]) {
+    for(int j = y * CELL_WIDTH; j < ((y + 1) * CELL_WIDTH); ++j) {
+      for(int i = x * CELL_WIDTH; i < ((x + 1) * CELL_WIDTH); ++i) {
+        simPutPixel(i, j, 0xFFFFFFFF);
+      }
+    }
+  }
+  else {
+    for(int j = y * CELL_WIDTH; j < ((y + 1) * CELL_WIDTH); ++j) {
+      for(int i = x * CELL_WIDTH; i < ((x + 1) * CELL_WIDTH); ++i) {
+        simPutPixel(i, j, 0xFF000000);
+      }
+    }
+  }
+  return;
+}
+
+void game_step() {
+  int neighbors_count;
+
+  for(int y = 0; y < FIELD_Y_SIZE; ++y) {
+    for (int x = 0; x < FIELD_X_SIZE; ++x) {
+      neighbors_count = 0;
+
+      if (x > 0) {
+        if (y > 0)
+          neighbors_count += (*prev_game_field)[x - 1][y - 1];
+
+          neighbors_count += (*prev_game_field)[x - 1][y    ];
+
+        if (y < FIELD_Y_SIZE - 1)
+          neighbors_count += (*prev_game_field)[x - 1][y + 1];
+      }
+
+      if (x < FIELD_X_SIZE - 1) {
+        if (y > 0)
+          neighbors_count += (*prev_game_field)[x + 1][y - 1];
+
+          neighbors_count += (*prev_game_field)[x + 1][y    ];
+
+          if(y < FIELD_Y_SIZE - 1)
+          neighbors_count += (*prev_game_field)[x + 1][y + 1];
+      }
+
+      if(y > 0)
+          neighbors_count += (*prev_game_field)[x    ][y - 1];
+
+      if(y < FIELD_Y_SIZE - 1)
+          neighbors_count += (*prev_game_field)[x    ][y + 1];
+
+      if((*prev_game_field)[x][y]) {
+        (*game_field)[x][y] = (neighbors_count == 2) || (neighbors_count == 3);
+      }
+      else {
+        (*game_field)[x][y] = (neighbors_count == 3);
+      }
+    }
+  }
+}
+
 void app(void) {
-  for (int step = 0; step < 1000; ++step) {
-    for (int y = 0; y < SIM_Y_SIZE; ++y)
-      for (int x = 0; x < SIM_X_SIZE; ++x)
-        simPutPixel(x, y, 0xFF000000 + x * y * step);
+  game_field      = &game_field_1;
+  prev_game_field = &game_field_2;
+
+  for (int y = 0; y < FIELD_Y_SIZE; ++y) {
+    for(int x = 0; x < FIELD_X_SIZE; ++x) {
+      (*prev_game_field)[x][y] = simRand() % 2;
+    }
+  }
+
+  while(1) {
+    game_step();
+    for (int y = 0; y < FIELD_Y_SIZE; ++y) {
+      for(int x = 0; x < FIELD_X_SIZE; ++x) {
+        draw_rectangle(x, y);
+      }
+    }
+
+    int (*swap)[FIELD_X_SIZE][FIELD_Y_SIZE];
+    swap = game_field;
+    game_field = prev_game_field;
+    prev_game_field = swap;
+
     simFlush();
   }
 }
--- SDL/sim.h
+++ SDL/sim.h
@@ -1,8 +1,13 @@
 #ifndef SIM_H
 #define SIM_H
 
-#define SIM_X_SIZE 512
-#define SIM_Y_SIZE 256
+#define FIELD_X_SIZE 100
+#define FIELD_Y_SIZE 20
+
+#define CELL_WIDTH 5
+
+#define SIM_X_SIZE FIELD_X_SIZE * CELL_WIDTH
+#define SIM_Y_SIZE FIELD_Y_SIZE * CELL_WIDTH
 
 #ifndef __sim__
 void simInit();
